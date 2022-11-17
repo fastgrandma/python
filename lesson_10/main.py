e81ca8afe92aa3562921fe11e00c15432acfedc7
movavi_school <abcsmir.ant@yandex.ru>
2022-11-17 11:58:30 +0700

    18 ноября

---
+++ lesson_10/main.py
@@ -0,0 +1,31 @@
+# lst = [0, 1, 2, 3, 4, 5]
+#
+# for mega_anton in lst:
+#     print(mega_anton)
+#
+# for super_anton in range(0, 11): # от 0 до  10 включительно
+#     print(super_anton)
+#
+# x1 = int(input("число: "))
+# x2 = int(input("число: "))
+#
+# while x1 <= x2:
+#     print(x1)
+#     x1 += 1
+#
+# for igoryan in range(x1, x2, + 1):
+#     print(igor
+#     while True:
+#         try:
+#     level = int(input("ярусов: ")
+#     exceptValuError:
+#        print(числодавайпж)
+# while True:
+#     char = input("символ")
+#     if len (char) == 1:
+#         break
+#     for i in range(1, level+1):
+#
+#         print(" " * (level-1), end = " ")
+#         print(char * 1,end=" ")
+#
---
+++ lesson_10/vecelica.py
@@ -0,0 +1,20 @@
+#     import random
+#
+#     vocabulary = ['Anton', 'Alex', 'Alexandr', 'Arsalan', 'Danil', 'Kirill', 'Sergey', 'Nikolay', 'Nasty', 'Natasha', 'Ivan', 'Igor', 'Gosha', 'Galina', 'Olya', 'Oksana', 'Oleg', 'James', 'Bill']
+#
+#     word_answer = random.choice(vocabulary)
+#     word_display = []
+#     for _i in(len(word_answer)):
+# word_display.append("_")
+#
+#     print(word_display)
+#     life = 6
+#     counter = 0 # кол-во букв проявленыз
+#     letter_is_be = False
+#
+#     while life > 0 and counter != len(word_answer)
+#
+# letter - input("буква: ")
+#     for i in range (len(word_answer)
+#     if letter == word_answer[i]
+#
