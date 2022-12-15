a0755c1e476ab2627587edfee104913904aacd6d
movavi_school <nastya290909.icloud.com>
2022-12-15 11:56:42 +0700

    15 декабря

---
+++ lesson_13/main.py
@@ -0,0 +1,93 @@
+# множества
+# s = set()  # пустое множество
+# s1 = {1, 2, 3, 3}  # дубликаты или повторения исключаются
+# l = []
+# print(s1)
+
+# s2 = {"А", "Б", "В"}
+# print(s2)
+
+# s1 = {1, 2, 3, 4, 5}
+# s2 = {3, 4, 5, 6, 7}
+# print(s1.union(s2))  # обьеденение
+# print(s1 |s2)  # тоже обьеденение
+#
+# print(s1.intersection(s2))
+# print(s1 & s2)
+#
+# print(s1.difference(s2))
+# print(s1 - s2)
+#
+# print(s1.symmetric_difference(s2))
+# print(s1 ^ s2)
+#
+#
+#
+# d = {}
+# d1 = {"пи": 3.14,
+#       "преподователь": "антон",
+#       "Список дел": ["выжить", "ловить балдеж"],
+#       "словарь": {"Вл1": 1}}
+
+# print(d1["преподователь"])
+# print(d1["список дел"][1])
+# print(d1["словарь"]["вл1"])
+
+# from random import randint
+#
+# lst = []
+# for _ in range(5):
+#     lst.append(randint(1, 5))
+# print(lst)
+#
+# unique = set(lst)
+# print(f"{len(unique)} штук: {unique}")
+
+
+# from random import randint
+#
+# lst1 = []
+# lst2 = []
+#
+# size = randint(100, 1000)
+# r_start = 0
+# r_end = 10_000  # декоротивный разделитель
+#
+#
+#
+#
+# for _ in range(size):
+#     lst1.append(randint(r_start, r_end))
+#     lst2.append(randint(r_start, r_end))
+# set1 = set(lst1)
+# set2 = set(lst2)
+#
+# inter = set1.intersection(set2)
+# print(f"общих чисел: {len(inter)}")
+# print(f"[Кол-во генераций]: {size}")
+# print(f"[минимальное значение]: {min(inter)}")
+# print(f"[ьаксимальное значение]: {max(inter)}")
+# inter1 = list(inter)
+# inter1.sort()
+# print(inter1)
+
+
+set1 = set()
+insert = ""
+while insert != "end"
+    insert = input("ввод: ")
+    if insert.lstrip("-").isdigit
+    if insert.isdigit():
+       if insert not in set1:
+          print("нет")
+          set1.add(insert)
+       else:
+          print("да")
+    elif insert == "end":
+        break
+    else:
+        print("")
+
+
+
+
