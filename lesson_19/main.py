e98a58d834fc65d7e3ae935b9ba27d99c5e0c1db
movavi_school <nastya290909.icloud.com>
2023-02-09 11:59:49 +0700

    f

--- lesson_19/main.py
+++ lesson_19/main.py
@@ -1,14 +1,26 @@
+import easygui
 def atbash(text):
-    alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-    reversed_alphabet = alphabet[::-1]
-    itog = ""
+    engin="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    reversed_eng = engin[::-1]
+    rusya="АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+    reversed_rus = rusya[::-1]
+    itog=""
     for letter in text:
-        itog += letter
-    c = alphabet.index(letter)
-    b = reversed_alphabet[c]
-    itog = itog + b
-
-
-print(itog)
-shifr = input("веди сообщение:").upper()
+        if letter not in rusya and letter not in engin:
+            itog += letter
+        elif letter in rusya:
+            indecs =rusya.index(letter)
+            bukva =reversed_rus[indecs]
+            itog=itog+bukva
+        else:
+            indecs = engin.index(letter)
+            bukva = reversed_eng[indecs]
+            itog = itog + bukva
+    easygui.msgbox(
+        msg=itog
+    )
+#    print(itog)
+shifr = easygui.enterbox(
+    msg="Введи сообщение, буба").upper()
+#shifr=input("Введи сообщение:").upper()
 atbash(shifr)
